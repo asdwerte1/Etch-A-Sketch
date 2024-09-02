53a62d573507213e713beda749850b811fb08815
asdwerte1 <samjmarkham@outlook.com>
2024-09-02 17:14:37 +0100

    Created 16 divs - each unique ID

---
+++ index.js
@@ -0,0 +1,8 @@
+addEventListener("DOMContentLoaded", () => {
+    const grid = document.getElementById("grid");
+    for (let index = 0; index < 16; index++) {
+        const element = document.createElement("div");
+        element.id = `${index + 1}`;
+        grid.appendChild(element);
+    }
+});
