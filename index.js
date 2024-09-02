b0e8b44866028798a40537e12c5d9f117e476c98
asdwerte1 <samjmarkham@outlook.com>
2024-09-02 22:15:21 +0100

    updated styling and able to change pixel size

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 addEventListener("DOMContentLoaded", () => {
+
     const grid = document.getElementById("grid");
-    for (let index = 0; index < 16; index++) {
-        const element = document.createElement("div");
-        element.id = `${index + 1}`;
-        element.className = "pixel";
-        element.addEventListener("mouseenter", () => {
-            element.style.backgroundColor = "#000";
-        });
-        grid.appendChild(element);
+    const dimension = parseInt(window.getComputedStyle(grid).width);
+
+    function generatePixels(number) {
+        
+        for (let index = 0; index < number**2; index++) {
+            console.log(index);
+            const element = document.createElement("div");
+            element.id = `${index + 1}`;
+            element.className = "pixel";
+            element.style.width = `${dimension / number}px`;
+            element.style.height = `${dimension / number}px`;
+
+            element.addEventListener("mouseenter", () => {
+                element.style.backgroundColor = "#000";
+            });
+            grid.appendChild(element);
+        }
     }
 
+    // Default case
+    generatePixels(4);
+    
     const resBtn = document.getElementById("res-btn");
     resBtn.addEventListener("click", () => {
-        prompt("Enter the desired resolution of the sketch area\nYour value should be in pixels and will set the width and height of the pixels");
+        const newRes = Number(prompt("Enter the desired resolution of the sketch area\nYour value should be in pixels and will set the width and height of the pixels"));
+        if (newRes > 100) {
+            alert("ERROR: Values above 100 are not permitted!");
+        } else if (!Number.isInteger(newRes)) {
+            alert("ERROR: Please ensure you enter a number!");
+        } else {
+            // Remove current pixels
+            const pixels = document.querySelectorAll(".pixel");
+            pixels.forEach(element => {
+                element.remove();
+            });
+            
+            generatePixels(newRes);
+        }
     })
 });
